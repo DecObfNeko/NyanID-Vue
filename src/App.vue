1f1bb627888de8c227e801eabbfae02c59d703b5
MiaoNai <wxyhxlmc@outlook.com>
2025-01-23 20:24:12 +0800

    upload

---
+++ src/App.vue
@@ -0,0 +1,40 @@
+<template>
+  <body name="app" class="app">
+    <h1>{{a}}</h1>
+    <button v-on:click="test()">clickme</button>
+
+  </body>
+
+
+
+</template>
+
+
+<script name="App" lang="ts" setup>
+import { reactive } from 'vue';
+
+let a = reactive([])
+
+function test() {
+ Object.assign(a, ["asdasdasdasd"])
+}
+
+
+</script>
+
+
+
+
+
+<script lang="ts">
+
+</script>
+
+
+
+
+
+
+<style scoped>
+
+</style>
