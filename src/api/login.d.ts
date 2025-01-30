6c117fb24d8343e9239508200e2e37e5cf0e1e90
icelly-QAQ <1787522500@qq.com>
2025-01-30 21:30:10 +0800

    add avatar
    添加了头像

--- src/api/login.d.ts
+++ src/api/login.d.ts
@@ -1,6 +1,8 @@
 import config from '@/config/configenv.d.ts';
 import axios from 'axios';
 
+axios.defaults.withCredentials = true;
+
 export const login = async (email, password) => {
     try {
         const response = await axios ({
--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -67,7 +67,12 @@
               d="M21.64,13a1,1,0,0,0-1.05-.14,8.05,8.05,0,0,1-3.37.73A8.15,8.15,0,0,1,9.08,5.49a8.59,8.59,0,0,1,.25-2A1,1,0,0,0,8,2.36,10.14,10.14,0,1,0,22,14.05,1,1,0,0,0,21.64,13Zm-9.5,6.69A8.14,8.14,0,0,1,7.08,5.22v.27A10.15,10.15,0,0,0,17.22,15.63a9.79,9.79,0,0,0,2.1-.22A8.11,8.11,0,0,1,12.14,19.73Z" />
           </svg>
         </label>
-        <RouterLink  class="btn" to="/login">Login</RouterLink>
+        <RouterLink  class="btn" to="/login" v-if="!isLogin">Login</RouterLink>
+          <div class="avatar" v-if="isLogin" to="/UserHome">
+            <div class="w-10 rounded-xl">
+              <img :src="avatarUrl" alt="User Avatar" />
+            </div>
+          </div>
       </div>
     </div>
   </header>
@@ -88,12 +93,16 @@ header {
 import { ref, onMounted, onUnmounted } from 'vue'
 import { getUserInfo } from '@/api/userInfo.d'
 import Cookies from 'js-cookie'
+import config from '@/config/configenv.d'
 
 const headerClass = ref('glass')
 const navbarClass = ref('glass')
 
+const isLogin = ref(false)
+
+const avatarUrl = ref('')
+
 const LoginToken = Cookies.get('LoginToken')
-console.error(LoginToken);
 
 const handleScroll = () => {
   if (window.scrollY > 0) {
@@ -107,7 +116,8 @@ const handleScroll = () => {
 
 getUserInfo(LoginToken).then(res => {
   if (res.status === 200) {
-    console.log(res)
+    isLogin.value = true
+    avatarUrl.value = `${config}/api/zako/res/avatar/${res.data.uid}`
   }
 })
 
