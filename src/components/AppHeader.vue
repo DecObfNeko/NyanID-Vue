4b372526de239eb6d85963406f9c02673f8ec278
icelly-QAQ <1787522500@qq.com>
2025-01-31 19:27:07 +0800

    Update AppHeader.vue

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -125,7 +125,6 @@ getUserInfo(LoginToken).then(res => {
     avatarUrl.value = `${config}/api/zako/res/avatar/${res.data.uid}`
     UserName.value = res.data.nickname
     isDeveloper.value = res.data.isDeveloper
-    console.log(res.data)
   }
 })
 
